215decd0caf55f59e58a04d88fca9018394330bf
Kaue Sloboda <kaue.sloboda@hotmail.com>
2023-04-25 16:55:36 -0300

    Instalando o react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { BrowserRouter } from "react-router-dom";
 import Inicio from "./paginas/Inicio/Index"
 import SobreMim from "./paginas/SobreMim/Index"
 
-console.log(window.location)
+// console.log(window.location)
 
 //pathname do window.location é o nome da página que estamos acessando
-const pagina = window.location.pathname === '/' ? <Inicio /> : <SobreMim />
+// const pagina = window.location.pathname === '/' ? <Inicio /> : <SobreMim />
 
 function App() {
-  return pagina;
+  return(
+    <BrowserRouter>
+      
+    </BrowserRouter>
+  );
 }
 
 export default App;
