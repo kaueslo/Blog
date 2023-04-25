ee343d18ec7ed37e84481d6f98321aeb142dd8a2
Kaue Sloboda <kaue.sloboda@hotmail.com>
2023-04-25 20:20:22 -0300

    alterando do Link para o NavLink no componente MenuLink

--- src/Componentes/MenuLink/Index.jsx
+++ src/Componentes/MenuLink/Index.jsx
@@ -1,22 +1,25 @@
-import { Link, useLocation } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import styles from "./MenuLink.module.css";
 
 export default function MenuLink({ children, to}) {
     //useLocation é o hook do reactrouterdom, vai retornar algo q tá sendo atribuído da localização
     //ele retorna o pathname tbm
-    const localizacao = useLocation();
+   // const localizacao = useLocation();
+   // coloca esse código com o location, quando usa a tag Link
+   //${localizacao.pathname === to ? styles.linkDestacado : ""}
 
     //console.log(localizacao)
 
     //Usamos o Link, pois é do react-router-dom, pra página não recarregar
   return (
-    <Link
-      className={`
-                    ${styles.link}
-                    ${localizacao.pathname === to ? styles.linkDestacado : ""}
-                `}
-      to={to}>
+    <NavLink
+      className={({ isActive }) =>`
+        ${styles.link}
+        ${isActive ? styles.linkDestacado : ""}
+      `}
+      to={to}
+      end>
       {children}
-    </Link>
+    </NavLink>
   );
 }
