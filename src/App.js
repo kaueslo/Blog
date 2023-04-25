9b61145d3159648a17a82f375d9e416a0c2ab95a
Kaue Sloboda <kaue.sloboda@hotmail.com>
2023-04-25 16:50:19 -0300

    Iniciando os componentes react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Inicio from "./paginas/Inicio/Index"
+import SobreMim from "./paginas/SobreMim/Index"
+
+console.log(window.location)
+
+//pathname do window.location é o nome da página que estamos acessando
+const pagina = window.location.pathname === '/' ? <Inicio /> : <SobreMim />
 
 function App() {
-  return (
-    <h1>Olá Mundo!</h1>
-  );
+  return pagina;
 }
 
 export default App;
